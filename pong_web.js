45168b6674440c016a97675524b3f50cb2291198
Joshua Dhallenne <jdhallen@c2r12p4.42lehavre.fr>
2025-08-18 15:38:48 +0200

    IA amelioration and player upgrade with smother move

--- pong_web.js
+++ pong_web.js
@@ -2,8 +2,9 @@
 const HEIGHT = 60;
 const WIDTH = 80;
 const MAX_BOUNCE_ANGLE = Math.PI / 5;
-const BALL_SPEED = 0.5;
+const BALL_SPEED = 0.7;
 const PLAYER_SPEED = 1;
+const MAX_SCORE = 10;
 
 //Web constant
 const canvas = document.getElementById('pong');
@@ -20,13 +21,22 @@ let player1 = Math.floor(HEIGHT / 2);
 let player2 = Math.floor(HEIGHT / 2);
 let ball = {x : WIDTH / 2, y : Math.floor(HEIGHT / 2), dx : Math.random() < 0.5 ? -1 : 1 , dy : Math.random() < 0.5 ? -1 : 1};
 
-//Data
-let velocity_use = 0;
+//Ball Movement
 let velocity1 = 0;
 let velocity2 = 0;
 let last_velocity1 = player1;
 let last_velocity2 = player2;
-let effect = 0.25;
+let effect = 0.15;
+
+//Player Movement
+let player1_vel = 0;
+let player2_vel = 0;
+let keyW = false, keyS = false, keyUp = false, keyDown = false;
+
+//Data
+let exchange_nbr = 0;
+let bounce_nbr = 0;
+let velocity_use = 0;
 
 //Size
 let top_margin_size = 1;
@@ -42,7 +52,7 @@ let ball_array = [];
 
 //IA
 let IA = true;
-let error_margin = 70;
+let error_margin = 65;
 let target_IA = {x : WIDTH / 2, y : Math.floor(HEIGHT / 2)};
 
 //Mode
@@ -54,14 +64,6 @@ let futur_vision = 60;
 let ball_futur = {x : WIDTH / 2, y : Math.floor(HEIGHT / 2), dx : Math.random() < 0.5 ? -1 : 1 , dy : Math.random() < 0.5 ? -1 : 1};
 let ball_array_futur = [];
 let ball_real_array_futur = [];
-
-//Keys
-// let player1_vel = 0;
-// let player2_vel = 0;
-// const PLAYER_MAX_SPEED = 0.6;
-// const PLAYER_ACCEL = 0.08;
-// const PLAYER_FRICTION = 0.85;
-// let keyW = false, keyS = false, keyUp = false, keyDown = false;
 
 function count_array_futur(touch){
 	for (let x = ball_futur.x - ball_size + 1; x < ball_futur.x + ball_size; x++){
@@ -79,9 +81,15 @@ function count_array_futur(touch){
 function move_obj_ball(obj_ball){
 	obj_ball.x += obj_ball.dx;
 	obj_ball.y += obj_ball.dy;
-	if (obj_ball.y <= ball_size + top_margin_size || obj_ball.y >= HEIGHT - (ball_size + top_margin_size)) obj_ball.dy *= -1;
+	if (obj_ball.y <= ball_size + top_margin_size || obj_ball.y >= HEIGHT - (ball_size + top_margin_size)){
+		if (obj_ball === ball)
+			bounce_nbr++;
+		obj_ball.dy *= -1;
+	}
 	if (obj_ball.dx < 0 && obj_ball.x <= (ball_size + bounce_margin_size) && obj_ball.x >= kill_margin_size
 			&& player1_array.includes(Math.round(obj_ball.y))){
+		if (obj_ball === ball)
+			exchange_nbr++;
 		let hit_pos = (obj_ball.y - player1) / ((player_size * 2 + 1) / 2);
 		if (hit_pos < -1) hit_pos = -1;
 		if (hit_pos > 1) hit_pos = 1;
@@ -91,12 +99,14 @@ function move_obj_ball(obj_ball){
 		if (obj_ball.dx < 0) obj_ball.dx = Math.abs(obj_ball.dx);
 		// Velocity from player
 		obj_ball.dy += velocity1 * effect
-		if (velocity1 != 0)
+		if (velocity1 != 0 && obj_ball === ball)
 			velocity_use++;
 
 	}
 	else if (obj_ball.dx > 0 && obj_ball.x >= WIDTH - (ball_size + bounce_margin_size) && obj_ball.x <= WIDTH - kill_margin_size
 			&& player2_array.includes(Math.round(obj_ball.y))){
+		if (obj_ball === ball)
+			exchange_nbr++;
 		let hit_pos = (obj_ball.y - player2) / ((player_size * 2 + 1) / 2);
 		if (hit_pos < -1) hit_pos = -1;
 		if (hit_pos > 1) hit_pos = 1;
@@ -106,7 +116,7 @@ function move_obj_ball(obj_ball){
 		if (obj_ball.dx > 0) obj_ball.dx = -Math.abs(obj_ball.dx);
 		// Velocity from player
 		obj_ball.dy += velocity2 * effect
-		if (velocity1 != 0)
+		if (velocity1 != 0 && obj_ball === ball)
 			velocity_use++;
 	}
 }
@@ -129,28 +139,42 @@ function futur(){
 }
 
 function searchIA(){
-	if (ball_array_futur.includes(target_IA)) return ;
 	let lowerindex = -1;
-	for (let i = error_margin; i < WIDTH - (bounce_margin_size); i++){
+	for (let i = error_margin; i < WIDTH - (kill_margin_size); i++){
 		for (let j = 0; j < HEIGHT - 1; j++){
 			let index = ball_array_futur.findIndex(b => b.x === i && b.y === j && b.touch == false);
 			if (index != -1){
-				if (lowerindex == -1 || index < lowerindex)
+				if (lowerindex == -1 || index > lowerindex)
 					lowerindex = index;
 			}
 		}
 	}
-	target_IA = {x : WIDTH / 2, y : Math.floor(HEIGHT / 2)};
+	target_IA = {x : WIDTH - bounce_margin_size, y : Math.floor(HEIGHT / 2)};
 	if (lowerindex == -1)
 		return ;
 	target_IA = ball_array_futur[lowerindex];
 }
 
 function moveIA(){
-	if (player2_array.includes(target_IA.y)) return ;
-	if (target_IA.y > player2 && player2 < HEIGHT - (player_size + top_margin_size)) player2++;
-	if (target_IA.y < player2 && player2 > player_size + top_margin_size) player2--;
-	// searchIA();
+	let ia_array = [];
+	let ia_player_size = (player_size <= 1) ? (player_size) : (player_size - 1);
+	for (let i = 0 - ia_player_size; i <= ia_player_size; i++){
+		ia_array.push(Math.round(player2) + i);
+	}
+	if (ia_array.includes(target_IA.y)) {
+		keyDown = false;
+		keyUp = false;
+		player2_vel = 0;
+		return ;
+	}
+	if (target_IA.y > player2){ // && player2 < HEIGHT - player_size){
+		keyUp = true;
+		player2_vel = 1 * PLAYER_SPEED;
+	}
+	if (target_IA.y < player2){//&& player2 > player_size){
+		keyDown = true;
+		player2_vel = -1 * PLAYER_SPEED;
+	}
 }
 
 function count_array_web(){
@@ -172,9 +196,25 @@ function draw_web(){
 	count_array_web();
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = "#FFFFFF";
-	for (let i = 2.5; i < HEIGHT; i += 5) {
+	for (let i = 1.5; i < HEIGHT; i += 5) {
 		ctx.fillRect((WIDTH/2 - 1) * SCALE_X, i * SCALE_Y, 2 * SCALE_X, 2 * SCALE_Y);
 	}
+	if (vision == true && IA == true){
+		ctx.fillStyle = "#001111";
+		for (let i = error_margin; i < WIDTH - (kill_margin_size ); i++){
+			for (let j = 0; j < HEIGHT; j += 1) {
+				ctx.fillRect(i * SCALE_X, j * SCALE_Y, SCALE_X, SCALE_Y);
+			}
+		}
+		ctx.fillStyle = "#00FFFF";
+		const ibx = target_IA.x * SCALE_X;
+		const iby = target_IA.y * SCALE_Y;
+		const iball_px = ball_size * 3 * SCALE_X;
+		ctx.beginPath();
+		ctx.arc(ibx, iby, iball_px / 2, 0, 2 * Math.PI);
+		ctx.fill();
+	}
+	ctx.fillStyle = "#FFFFFF";
 	ctx.fillRect(0, 0, canvas.width, SCALE_Y);
 	ctx.fillRect(0, (HEIGHT-1) * SCALE_Y, canvas.width, SCALE_Y);
 	if (vision == true){
@@ -199,6 +239,26 @@ function draw_web(){
 	ctx.beginPath();
 	ctx.arc(bx, by, ball_px / 2, 0, 2 * Math.PI);
 	ctx.fill();
+}
+
+function movePlayer(){
+	if (keyW == true && player1 > player_size + top_margin_size)
+		player1 += player1_vel;
+	if (keyS == true && player1 < HEIGHT - (player_size + top_margin_size))
+		player1 += player1_vel;
+	if (keyUp == true && player2 > player_size + top_margin_size)
+		player2 += player2_vel;
+	if (keyDown == true && player2 < HEIGHT - (player_size + top_margin_size))
+		player2 += player2_vel;
+
+	if (player2 >= HEIGHT - (player_size + top_margin_size))
+		player2--;
+	if (player2 <= player_size + top_margin_size)
+		player2++;
+	if (player1 >= HEIGHT - (player_size + top_margin_size))
+		player1--;
+	if (player1 <= player_size + top_margin_size)
+		player1++;
 }
 
 function moveBall(){
@@ -226,43 +286,120 @@ function moveBall(){
 }
 
 document.addEventListener('keydown', function(event) {
-	key = event.key;
-	if (key === 'w' && player1 > player_size + top_margin_size) player1 -= 1 * PLAYER_SPEED;
-	if (key === 's' && player1 < HEIGHT - (player_size + top_margin_size)) player1 += 1 * PLAYER_SPEED;
-	if (key === 'ArrowUp' && player2 > player_size + top_margin_size && IA == false) player2 -= 1 * PLAYER_SPEED;
-	if (key === 'ArrowDown' && player2 < HEIGHT - (player_size + top_margin_size) && IA == false) player2 += 1 * PLAYER_SPEED;
+	let key = event.key;
+	if (key === 'w' && player1 > player_size + top_margin_size){
+		player1_vel = -1 * PLAYER_SPEED;
+		keyS = false;
+		keyW = true;
+	} 
+	if (key === 's' && player1 < HEIGHT - (player_size + top_margin_size)){
+		player1_vel = 1 * PLAYER_SPEED;
+		keyW = false;
+		keyS = true;
+	}
+	if (key === 'ArrowUp' && player2 > player_size + top_margin_size && IA == false){
+		player2_vel = -1 * PLAYER_SPEED;
+		keyDown = false;
+		keyUp = true;
+	}
+	if (key === 'ArrowDown' && player2 < HEIGHT - (player_size + top_margin_size) && IA == false){
+		player2_vel = 1 * PLAYER_SPEED;
+		keyUp = false;
+		keyDown = true;
+	}
 	if (key === 'p' && pause == true) pause = false;
 	else if (key === 'p' && pause == false) pause = true;
 	if (key === 'v' && vision == true) vision = false;
 	else if (key === 'v' && vision == false) vision = true;
 	if (key === ',' && vision == true) futur_vision -= 1;
 	if (key === '.' && vision == true) futur_vision += 1;
+	if (key === ';' && vision == true) error_margin -= 1;
+	if (key === '\'' && vision == true) error_margin += 1;
 	if (key === '1' && ball_size < 15) ball_size += 1;
 	if (key === '2' && ball_size > 1) ball_size -= 1;
 	if (key === '3' && player_size < 15) player_size += 1;
 	if (key === '4' && player_size > 1) player_size -= 1;
 	if (key === '\\' && IA == true) IA = false;
 	else if (key === '\\' && IA == false) IA = true;
+	if (key === '+' || key === '-'){
+		let angle = Math.atan2(ball.dy, ball.dx) * 180 / Math.PI;
+		angle += (key === '+') ? 1.5 : -1.5;
+		let speed = Math.sqrt(ball.dx * ball.dx + ball.dy * ball.dy) || BALL_SPEED;
+		ball.dx = speed * Math.cos(angle * Math.PI / 180);
+		ball.dy = speed * Math.sin(angle * Math.PI / 180);
+	}
 });
 
-function afficherMessage(msg) {
+document.addEventListener('keyup', function(event) {
+	let key = event.key;
+	if (key === 'w'){
+		keyW = false;
+		if (keyS === false)
+			player1_vel = 0;
+	}
+	if (key === 's'){
+		keyS = false;
+		if (keyW === false)
+			player1_vel = 0;
+	}
+	if (IA == false){
+		if (key === 'ArrowUp'){
+			keyUp = false;
+			if (keyDown === false)
+				player2_vel = 0;
+		}
+		if (key === 'ArrowDown'){
+			keyDown = false;
+			if (keyUp === false)
+				player2_vel = 0;
+		}
+	}
+});
+
+function afficherMessage(msg, side) {
     ctx.font = "40px Arial";
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillText(msg, 100, canvas.height/2);
+	 let stats = [
+        "STATS :",
+        "exchange_nbr : " + exchange_nbr,
+        "bounce nbr : " + bounce_nbr,
+        "velocity boost nbr : " + velocity_use
+    ];
+	let msgX, statsX;
+	let lineHeight = 40;
+    if (side == 'l') {
+        msgX = 10;
+        statsX = canvas.width - 10 - ctx.measureText(stats[0]).width; // aligner à droite
+    } else if (side == 'r') {
+        msgX = canvas.width - 10 - ctx.measureText(msg).width; // aligner à droite
+        statsX = 10;
+    }
+    let blockTop = canvas.height / 2 - (stats.length * lineHeight) / 2;
+	
+	ctx.fillStyle = "#AAAAAA";
+	ctx.fillText(msg, msgX, (canvas.height / 2) + (lineHeight / 2));
+
+	for (let i = 0; i < stats.length; i++) {
+		let text = stats[i];
+		let y = blockTop + i * lineHeight;
+		ctx.fillStyle = "#AAAAAA";
+		ctx.fillText(text, statsX, y);
+	}
 }
 
 let gameover = false;
 function gameLoop() {
-	if (player1_score >= 10){
+	if (player1_score >= MAX_SCORE){
 		gameover = true;
-		afficherMessage("Player 1 Wins !!!");
+		afficherMessage("Player 1 Wins !!!", 'l');
 		return ;
 	}
-	else if (player2_score >= 10){
+	else if (player2_score >= MAX_SCORE){
 		gameover = true;
-		afficherMessage("Player 2 Wins !!!");
+		afficherMessage("Player 2 Wins !!!", 'r');
 		return ;
 	}
+	movePlayer();
 	if (pause == false)
 		moveBall();
 	ball_array_futur = [];
@@ -282,7 +419,7 @@ setInterval(() => {
 	velocity2 = player2 - last_velocity2;
 	last_velocity1 = player1;
 	last_velocity2 = player2;
-}, 150);
+}, 100);
 
 setInterval(() => {
 	if (IA == true)
